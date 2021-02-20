0a5d2e381d4659981443c024d450f6c2b345c666
kelevo <patrickjhonatanh@gmail.com>
2021-02-19 19:03:13 -0600

    valores y variables en JS

---
+++ variables.js
@@ -0,0 +1,21 @@
+var nombre = "Patrick"
+
+//Declarar una variable
+var edad;
+
+//Inicializar la variable
+edad = 30;
+
+//Variables de array
+var elementos = ["Computadora", "Celular"];
+//Mandar una variable array
+elementos
+elementos[1]
+
+//Variable tipo objeto
+var persona = {
+    nombre: "Diego",
+    edad: 30
+}
+//Mandar a llamar una variable tipo objeto
+persona
